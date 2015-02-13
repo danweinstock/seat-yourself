e5fa0b727087ed65da0464665f73e474bfb823be
danweinstock <dan.weinstock@yahoo.com>
2015-02-13 14:40:16 -0500

    user and restaurant controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
 class RestaurantsController < ApplicationController
+
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def show
+		@restaurant = Restaurant.find[(:id)]
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.new(restaurant_params)
+
+		if @restaurant.save
+			redirect_to restaurants_url
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :address, :phone_number, :picture, :description, :cuisine, :capacity, :neighbourhood)
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+	resources :users
+	resources :restaurants
+	resources :reservations
+
 end
